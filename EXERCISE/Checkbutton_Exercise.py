67f0b1cb76509b2e4510d8b2ee2f29810436b65c
512439 <512439@my.cwc.ac.uk>
2025-09-30 11:04:50 +0100

    checkbutton

--- EXERCISE/Checkbutton_Exercise.py
+++ EXERCISE/Checkbutton_Exercise.py
@@ -8,10 +8,10 @@ window.geometry('400x400')
 
 # this ius our Checknbutton
 bool_var = tk.BooleanVar()
-Boolean_checkbutton = ttk.Checkbutton(window, variable = bool_var, text = 'I am very HAPPY for you!', onvalue = True)
+Boolean_checkbutton = ttk.Checkbutton(window, variable = bool_var, text = 'Are you happy?', onvalue = True)
 
 #Pack elements in frames ready to push onto form/window
 Boolean_checkbutton.pack()
 
 # run the program to generate window with all packed elements for user interaction 
-window.mainloop() 
+window.mainloop()
