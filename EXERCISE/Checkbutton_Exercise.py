478267f23551b3ff801ae7150b3d97ee8b6b0b7c
512439 <512439@my.cwc.ac.uk>
2025-09-30 10:54:29 +0100

    checkbutton

---
+++ EXERCISE/Checkbutton_Exercise.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+
+# This section creaetes the Tkinter Window and adds the required elements to it
+window = tk.Tk()
+window.title('Tkinter Checkbutton Widget')
+window.geometry('400x400')
+
+# this ius our Checknbutton
+bool_var = tk.BooleanVar()
+Boolean_checkbutton = ttk.Checkbutton(window, variable = bool_var, text = 'Are you sad?', onvalue = True)
+
+#Pack elements in frames ready to push onto form/window
+Boolean_checkbutton.pack()
+
+# run the program to generate window with all packed elements for user interaction 
+window.mainloop() 
