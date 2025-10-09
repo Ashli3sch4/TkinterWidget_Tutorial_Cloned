44728c714d74463277f9ff69daf075e85c69df5f
512439 <512439@my.cwc.ac.uk>
2025-10-09 09:20:25 +0100

    Combobox

--- EXERCISE/Combobox_Exercise.py
+++ EXERCISE/Combobox_Exercise.py
@@ -1,18 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
- 
-#window
+
 window = tk.Tk()
 window.title('Tkinter Combobox Widget')
 window.geometry('400x400')
- 
-#combobox widget
+
 string_var = tk.StringVar()
-myCombobox = ttk.Combobox(master = window, textvariable = string_var, values=['This Options', 'That Options', 'Another Option'])
- 
-#pack
+myCombobox = ttk.Combobox(master = window, textvariable = string_var,values=['This Option', 'That Option', 'Another Option'])
+
 myCombobox.pack()
- 
-#run
+
 window.mainloop()
- 
